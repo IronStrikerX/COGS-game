8e6ea18a1b79fde109da8691f3bb8f42b1221d6b
Andy Ling <andy.siyuan.ling@gmail.com>
2025-10-22 02:25:52 -0400

    Squashed commit of the following:
    commit e2dde27de9418c4ab7abf724eb7ce0e07b4eec9f
    Author: Andy Ling <andy.siyuan.ling@gmail.com>
    Date:   Wed Oct 22 02:19:44 2025 -0400
    
        don't even know what i did
    
    commit fe2422c2cc51afd1d039b0cca75cecb3e5c54f4f
    Author: Andy Ling <andy.siyuan.ling@gmail.com>
    Date:   Wed Oct 22 00:54:20 2025 -0400
    
        makes sure each appliance is accessable

--- Game/character_body_2d.gd
+++ Game/character_body_2d.gd
@@ -30,9 +30,7 @@ func _physics_process(_delta: float) -> void:
 	
 	velocity = mov.normalized() * SPEED
 	move_and_slide()
-	if Input.is_action_just_pressed("debug"):
-		print(current_customer)
-		
+
 	if Input.is_action_just_pressed(prefix + "action"):
 		# Pick up food
 		if not selected_food and object:
@@ -74,6 +72,11 @@ func _physics_process(_delta: float) -> void:
 				if current_customer.receive_food(selected_food):
 					selected_food = null
 					selected_food_sprite.texture = null
+		
+	if global_position.y > get_viewport_rect().size.y:
+		global_position.y = 0
+	elif global_position.y < 0:
+		global_position.y = get_viewport_rect().size.y
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent().is_in_group("food"):
