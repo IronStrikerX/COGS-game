65322ccb5b3504bcdc7531ad92f491ef0007c96e
Andy Ling <andy.siyuan.ling@gmail.com>
2025-10-22 22:09:51 -0400

    drop ability

--- Game/character_body_2d.gd
+++ Game/character_body_2d.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 const SPEED = 500.0
+const DROP_TIME = 1.5
 
 @export var player_id: int
 
 @onready var selected_food_sprite: Sprite2D = %SelectedFood
 @onready var player_sprite: Sprite2D = $PlayerSprite
+@onready var drop_bar: ProgressBar = $DropBar
+@onready var drop_timer: Timer = $DropTimer
 
 var object: Area2D = null
 var nearby_appliances: Array[StaticBody2D] = []
@@ -13,6 +16,11 @@ var current_appliance: StaticBody2D = null
 var current_customer: Area2D = null
 
 var selected_food: FoodResource = null
+var holding_drop: bool = false
+var hold_time: float = 0.0
+var hold_buffer: float = 0.2
+
+var try_swap_already := false
 
 func _ready():
 	match player_id:
@@ -22,61 +30,108 @@ func _ready():
 			player_sprite.texture = preload("uid://5fnneva0kop")
 			
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var prefix = "p%d_" % player_id
+	
+	# --- Movement ---
 	var x_mov = Input.get_action_strength(prefix + "right") - Input.get_action_strength(prefix + "left")
 	var y_mov = Input.get_action_strength(prefix + "down") - Input.get_action_strength(prefix + "up")
 	var mov = Vector2(x_mov, y_mov)
-	
 	velocity = mov.normalized() * SPEED
 	move_and_slide()
-
+	
+	# --- Action Key ---
 	if Input.is_action_just_pressed(prefix + "action"):
-		# Pick up food
-		if not selected_food and object:
-			selected_food = object.get_parent().food
-			selected_food_sprite.texture = selected_food.texture
-		
-		# Drop food at station
-		elif selected_food and current_appliance:
-			if current_appliance.has_method("receive_food"):
+		# Single tap behavior: try instant swap/pickup
+		if not selected_food:
+			try_pickup()
+		elif selected_food:
+			# Attempt instant swap if appliance or customer supports it
+			if current_appliance and current_appliance.has_method("receive_food"):
 				if current_appliance.receive_food(selected_food, player_id):
-					#player will drop selected food only when 1. appliance is empty 2. player can cook with the 2 foods
-					selected_food = null
-					selected_food_sprite.texture = null
-				elif current_appliance.appliance_type in [CookingRecipe.ApplianceType.COUNTERTOP, CookingRecipe.ApplianceType.ASSEMBLY_STATION]:
-					# Could not cook, attempt swap if appliance has food to give on countertrop or assemblys tation
-					var appliance_food = current_appliance.give_food(player_id)
-					if appliance_food:
-						if current_appliance.receive_food(selected_food, player_id):
-							# Swap successful
-							selected_food = appliance_food
-							if selected_food:
-								selected_food_sprite.texture = selected_food.texture
-							else:
-								selected_food_sprite.texture = null
-			else:
-				print("Station can't receive food")
-									
-		#pick up food at station
-		elif not selected_food and current_appliance:
-			if current_appliance.has_method("give_food"):
-				var received = current_appliance.give_food(player_id)
-				if received:
-					selected_food = received
-					selected_food_sprite.texture = selected_food.texture
-					print("Picked up ", received.name, "from ", current_appliance.appliance_type)
-		
-		elif selected_food and current_customer:
-			if current_customer.has_method("receive_food"):
+					clear_selected_food()
+			elif current_customer and current_customer.has_method("receive_food"):
 				if current_customer.receive_food(selected_food):
-					selected_food = null
-					selected_food_sprite.texture = null
+					clear_selected_food()
+		# Start hold timer
+		holding_drop = true
+		hold_time = 0
 		
-	if global_position.y > get_viewport_rect().size.y:
-		global_position.y = 0
-	elif global_position.y < 0:
-		global_position.y = get_viewport_rect().size.y
+
+	elif Input.is_action_pressed(prefix + "action") and holding_drop and selected_food:
+		# Holding: increment timer
+		hold_time += delta
+		if hold_time >= hold_buffer:
+			drop_bar.visible = true
+			drop_bar.value = clamp(hold_time / DROP_TIME, 0, 1)
+			if hold_time >= DROP_TIME:
+				# Drop food after hold time
+				if current_appliance and current_appliance.has_method("receive_food"):
+					current_appliance.receive_food(selected_food, player_id)
+				clear_selected_food()
+				holding_drop = false
+				drop_bar.visible = false
+
+	elif Input.is_action_just_released(prefix + "action"):
+		# Reset hold state if released early
+		holding_drop = false
+		hold_time = 0
+		drop_bar.value = 0
+		drop_bar.visible = false
+
+	 
+
+
+# --- Pickup Logic ---
+func try_pickup():
+	# Pick up from object in world
+	if object:
+		selected_food = object.get_parent().food
+		selected_food_sprite.texture = selected_food.texture
+		return
+	
+	# Pick up from appliance
+	if current_appliance and current_appliance.has_method("give_food"):
+		var received = current_appliance.give_food(player_id)
+		if received:
+			selected_food = received
+			selected_food_sprite.texture = selected_food.texture
+			print("Picked up %s from %s" % [received.name, str(current_appliance.appliance_type)])
+			return
+
+
+# --- Drop / Swap Logic ---
+func try_swap():
+	# Drop at appliance
+	try_swap_already = true
+	if current_appliance and current_appliance.has_method("receive_food"):
+		if current_appliance.receive_food(selected_food, player_id):
+			# Drop successful
+			clear_selected_food()
+		elif current_appliance.appliance_type in [CookingRecipe.ApplianceType.COUNTERTOP, CookingRecipe.ApplianceType.ASSEMBLY_STATION]:
+			# Try swap
+			var appliance_food = current_appliance.give_food(player_id)
+			if appliance_food and current_appliance.receive_food(selected_food, player_id):
+				# Swap successful
+				selected_food = appliance_food
+				if selected_food:
+					selected_food_sprite.texture = selected_food.texture
+				else:
+					clear_selected_food()
+		return
+	
+	# Give to customer
+	if current_customer and current_customer.has_method("receive_food"):
+		if current_customer.receive_food(selected_food):
+			clear_selected_food()
+		return
+
+
+# --- Helper to clear food ---
+func clear_selected_food():
+	selected_food = null
+	selected_food_sprite.texture = null
+
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent().is_in_group("food"):
